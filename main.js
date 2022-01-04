aa40d8acd8f0d0098ed1f326d436d7ab10f782a4
YoonHyunWoo <88831669+YoonHyunWoo@users.noreply.github.com>
2022-01-04 14:30:07 +0900

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-var router_1=require('./router/router_1');
-
-app.use('/aboutrouter',router_1);
-
-app.listen(3000,()=>{
-    console.log('server is running to localhost:3000');
-});
